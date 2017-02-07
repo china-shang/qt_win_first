9b408d78deabdcfccce6ddfef28aa899197c419b
win10 <850747813@qq.com>
2017-02-07 15:41:10 +0800

    other thing

--- QT_5.7/main.cpp
+++ QT_5.7/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MyClass w;
     QPushButton *button = new QPushButton("quit", &w);
+    //other thing
     w.show();
     QObject:: connect(button,&QPushButton::pressed,&QApplication::quit);
     return a.exec();
